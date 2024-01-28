390994524905afe5f9960796ca94cd5d5115ca86
Deepak <jeenad36@gmail.com>
2024-01-28 19:18:55 +0530

    Making the Login form and Style it with css and js

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ faqs.forEach((faq) =>{
         faq.classList.toggle("active");
     });
 });
+
+let LoginButton = document.getElementById("mainloginbutton")
+function showparentContainer(){
+document.querySelector(".parentContainer").classList.add("showparentContainer")
+document.body.style.overflow = "hidden"
+}
+
+function showLogincontainer(){
+    document.querySelector(".Logincontainer").classList.add("showLoginContainer")
+}
+
+LoginButton.addEventListener("click" , ()=>{
+    showparentContainer()
+    showLogincontainer()
+})
